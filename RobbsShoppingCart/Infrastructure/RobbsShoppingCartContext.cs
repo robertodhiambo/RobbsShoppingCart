a4182120c4048f0a4565d7ee0b7844f03aaf4058
robertodhiambo <robertodhiambo661@gmail.com>
2022-08-29 12:09:30 +0300

    db

---
+++ RobbsShoppingCart/Infrastructure/RobbsShoppingCartContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RobbsShoppingCart.Infrastructure
+{
+    public class RobbsShoppingCartContext : DbContext
+    {
+        public RobbsShoppingCartContext(DbContextOptions<RobbsShoppingCartContext> options)
+            :base(options)
+        {
+                
+        }
+    }
+}
